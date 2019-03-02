a6fc1680fbf147800bb32741b00b14a5cf166999
rajan-gith <rajan.prajapati@berylsystems.com>
2019-03-02 16:23:56 +0530

    first ruby greeting program

---
+++ ruby/firstruby.rb
@@ -0,0 +1,8 @@
+print("Enter you name:")
+name=gets.strip
+print("Enter your age:")
+age=gets.strip
+print("Enter your city:")
+city=gets.strip
+
+puts("Hi there #{name}. You are #{age} yrs old from #{city}. ")
