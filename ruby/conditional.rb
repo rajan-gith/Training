c68036aca6d6fd0f1a11dfb33d55c6628e0a5b90
rajan-gith <rajan.prajapati@berylsystems.com>
2019-03-05 09:46:10 +0530

    condition with 'and' condition false.

---
+++ ruby/conditional.rb
@@ -0,0 +1,13 @@
+a = 3
+b = 2
+if a == 2 and b ==3
+	puts(" a is #{a} and b is #{b}")
+elsif a ==2 or b ==3
+	if a == 2
+		puts("you got a #{a}")
+	else
+		puts("you got b #{b}")
+	end
+else 
+	puts("you did not got any thing.")
+end
