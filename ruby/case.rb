4a68b437a6aa581afae6641303f9c996ec91dbed
rajan-gith <rajan.prajapati@berylsystems.com>
2019-03-05 10:44:37 +0530

    More Case statement

--- ruby/case.rb
+++ ruby/case.rb
@@ -7,3 +7,17 @@ when 5
 else 
 	puts("None")
 end
+name = 'Jack'
+case name
+when 'Jade'
+	puts("Hello Jade")
+when 'Jake'
+	puts("Hello Jake")
+when 'Jack'
+	puts("Hello Jack")
+else
+	puts("No match")
+end
+		
+		
+		
