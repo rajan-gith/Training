a66c6a24e48cb38e5f2413c3d360be03c0188f93
rajan-gith <rajan.prajapati@berylsystems.com>
2019-03-05 10:18:49 +0530

    Ifmodifier with unless

--- ruby/ifmodifieer.rb
+++ ruby/ifmodifieer.rb
@@ -1,3 +1,8 @@
 a = 3
 
-puts("#{a} is three ") if a == 3 
+unless a >= 4 
+	puts("Please increase the value of a")
+else 
+	puts("Yes it is 4 or more than 4 now")
+end
+puts("value of a is #{a}") unless a>=4
