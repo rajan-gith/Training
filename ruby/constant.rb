655fd1ca63bb02d0c5611fc38c5457b0a2d6a831
rajan-gith <rajan.prajapati@berylsystems.com>
2019-03-05 14:57:17 +0530

    constant

---
+++ ruby/constant.rb
@@ -0,0 +1,10 @@
+class Constant
+	CONSTANT1=100
+	CONSTANT2=200
+
+	def show
+		puts("#{CONSTANT1} and #{CONSTANT2} are the values of CONSTANT1 and CONSTANT2")
+	end
+end
+cost=Constant.new()
+cost.show
