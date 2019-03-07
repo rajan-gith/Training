abf0d4903ad35c11fb8a4dd223c7c3a093e95471
rajan-gith <rajan.prajapati@berylsystems.com>
2019-03-07 12:07:43 +0530

    timmings with ruby

--- ruby/time_formatting.rb
+++ ruby/time_formatting.rb
@@ -6,9 +6,44 @@ puts time.ctime
 
 
 
-# %a for short_name
-# %A for complete name
+# %a for abbreviated of week day
+# %A for complete name of week day
 
 p time.strftime("%A"),time.strftime("%a")
 
- 
+# %b for abbreviated name of month
+# %B for complete name of month
+
+p time.strftime("%b"), time.strftime("%B"), time.strftime("%c")
+# The preferred local date and time representation. by %c
+str= time.strftime("%c")
+p str.class
+print time.strftime("%I")+":"+ time.strftime("%M")+" "+time.strftime("%p")
+=begin 
+	some important keys
+	%H for 24 hrs clock
+	%I for 12 hrs clock
+	%p meridian indecation
+	%M for minute
+	%y year without centuary
+	%Y year with centuary
+
+=end
+
+
+# time arithmetic
+
+now = Time.now
+past = now - 10         # 10 seconds ago. Time - number => Time
+puts past
+
+future = now + 10  # 10 seconds from now Time + number => Time
+puts future
+
+puts now
+
+
+
+
+
+
