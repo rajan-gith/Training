f54ab696efba6795bb1b19c212fa967d0695c508
rajan-gith <rajan.prajapati@berylsystems.com>
2019-03-11 11:48:56 +0530

    assoc rassoc

---
+++ ruby/March 11/Arrrasoc.rb
@@ -0,0 +1,5 @@
+a = [ [ 1, "two", "one", "two"], [2, "two"], [3, "three"], ["ii", "two"] ]
+p a.rassoc("two") 
+p a.rassoc("four")
+
+#rassoc searches for the second item and assoc seraches in first Item.
