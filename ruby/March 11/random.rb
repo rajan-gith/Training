dada38c1a81839efdceeeda6ae60c7841ae61e52
rajan-gith <rajan.prajapati@berylsystems.com>
2019-03-11 18:05:29 +0530

    Playing with Random umbers

---
+++ ruby/March 11/random.rb
@@ -0,0 +1,11 @@
+puts rand # this will put a random number 
+
+# Random number from between 1 to 100
+
+puts rand*100
+
+# Random Integer numbers
+
+puts (rand*100).to_i
+
+puts rand(10) # ranges 0 to 10 
