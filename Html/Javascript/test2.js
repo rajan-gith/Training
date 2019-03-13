91bf784f389e704d0ad7454202ebbc218f7cc7af
rajan-gith <rajan.prajapati@berylsystems.com>
2019-03-13 11:51:26 +0530

    Arithmetic subtractions numbers

--- Html/Javascript/test2.js
+++ Html/Javascript/test2.js
@@ -2,3 +2,8 @@
  b=document.getElementById('two').innerHTML
 
 document.getElementById("result").innerHTML+= (a)+" "+(b)
+
+var x = 5;
+var y = 2;
+var z = x - y;
+document.getElementById("demo2").innerHTML = z
