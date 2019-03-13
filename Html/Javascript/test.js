794d41cff679ab4fe843099f6e3af1f5c4fac5e9
rajan-gith <rajan.prajapati@berylsystems.com>
2019-03-13 11:04:35 +0530

    Console Used

--- Html/Javascript/test.js
+++ Html/Javascript/test.js
@@ -1,3 +1,11 @@
 var a = document.getElementsByTagName('p')
 
 document.getElementById("demo").innerHTML = 5 + 6;
+
+window.alert("The content of paragraph tag has been changed")
+
+
+var a=4
+var b=5
+console.log(a+b)
+console.log("Above was the addition of variables a and b")
