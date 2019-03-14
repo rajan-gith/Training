201d580a76c158254882d033956fbb88e808ba53
rajan-gith <rajan.prajapati@berylsystems.com>
2019-03-14 16:40:04 +0530

    routes updated

--- rails/alpha-blog/config/routes.rb
+++ rails/alpha-blog/config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'student/index'
+  get 'student/edit'
+  get 'student/new'
+  get 'student/delete'
+  get 'student/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#index'
+  get 'about', to: 'welcome#about'
 end
