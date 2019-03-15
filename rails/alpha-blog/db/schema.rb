e23e9cc1dafb5721ebf44d49af71f7583007262c
rajan-gith <rajan.prajapati@berylsystems.com>
2019-03-15 17:02:36 +0530

    Added reference in players to users

--- rails/alpha-blog/db/schema.rb
+++ rails/alpha-blog/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_15_092608) do
+ActiveRecord::Schema.define(version: 2019_03_15_111839) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2019_03_15_092608) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "game_name", null: false
+    t.bigint "users_id"
+    t.index ["users_id"], name: "index_players_on_users_id"
   end
 
   create_table "users", force: :cascade do |t|
