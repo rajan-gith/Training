7c779331cd5751bff3af0226ac289cbc4d3740e1
rajan-gith <rajan.prajapati@berylsystems.com>
2019-03-15 11:31:12 +0530

    Renamed rollno column to username

--- rails/alpha-blog/db/schema.rb
+++ rails/alpha-blog/db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_15_054040) do
+ActiveRecord::Schema.define(version: 2019_03_15_055438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.integer "rollno"
+    t.integer "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", null: false
