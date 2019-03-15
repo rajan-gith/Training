fb7883e4229fe4386bd237921a88c4f86b869ca8
rajan-gith <rajan.prajapati@berylsystems.com>
2019-03-15 11:09:14 +0530

    Added extra column named detail for practice

--- rails/alpha-blog/db/schema.rb
+++ rails/alpha-blog/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_15_052314) do
+ActiveRecord::Schema.define(version: 2019_03_15_053532) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_03_15_052314) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", null: false
+    t.string "detail"
   end
 
 end
