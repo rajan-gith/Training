b78ec3177e846ef5e1017328f838386a8cd7f02c
rajan-gith <rajan.prajapati@berylsystems.com>
2019-03-18 10:01:00 +0530

    User and Players reference and fk constraint

---
+++ rails/Hostel_Management/db/migrate/20190315122713_adding_reference_only_user_to_player.rb
@@ -0,0 +1,5 @@
+class AddingReferenceOnlyUserToPlayer < ActiveRecord::Migration[5.2]
+  def change
+    add_reference(:players, :user)
+  end
+end
---
+++ rails/Hostel_Management/db/migrate/20190315122941_adding_fk_in_player.rb
@@ -0,0 +1,5 @@
+class AddingFkInPlayer < ActiveRecord::Migration[5.2]
+  def change
+    add_foreign_key(:players, :users)
+  end
+end
