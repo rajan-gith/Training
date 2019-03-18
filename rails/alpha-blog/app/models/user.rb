965e4dcc20ba82624b6495671f9d2fa29034b070
rajan-gith <rajan.prajapati@berylsystems.com>
2019-03-18 18:15:15 +0530

    Fixed User and Game model typo for comment

--- rails/alpha-blog/app/models/user.rb
+++ rails/alpha-blog/app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
   has_many :games, through: :enrollments
   has_one :purse
   has_one :payment_history, through: :purse
-  has_many :comment, as: :commentable
+  has_many :comments, as: :commentable
 end
