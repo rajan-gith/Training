cdef4163c50bbf5bea49e37315211926949041a8
rajan-gith <rajan.prajapati@berylsystems.com>
2019-03-18 14:55:19 +0530

    Created Address model and one to one relationship in address and user

--- rails/alpha-blog/db/schema.rb
+++ rails/alpha-blog/db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_18_082951) do
+ActiveRecord::Schema.define(version: 2019_03_18_090808) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "addresses", force: :cascade do |t|
+    t.string "street"
+    t.string "city"
+    t.string "country"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_addresses_on_user_id"
+  end
 
   create_table "players", force: :cascade do |t|
     t.string "game_name", null: false
