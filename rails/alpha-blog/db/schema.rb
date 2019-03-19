0e585363a0737d8f5b58cf17209f7c1476843263
rajan-gith <rajan.prajapati@berylsystems.com>
2019-03-19 11:59:05 +0530

    Removing not null conststraint from users table

--- rails/alpha-blog/db/schema.rb
+++ rails/alpha-blog/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_18_123443) do
+ActiveRecord::Schema.define(version: 2019_03_19_062526) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -102,8 +102,8 @@ ActiveRecord::Schema.define(version: 2019_03_18_123443) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "username", null: false
+    t.string "name"
+    t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", null: false
