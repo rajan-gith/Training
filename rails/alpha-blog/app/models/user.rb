4ee20426219257b9023dbc300f603cbff0f32be9
rajan-gith <rajan.prajapati@berylsystems.com>
2019-03-20 14:09:18 +0530

    Added uniqueness to email in User model

--- rails/alpha-blog/app/models/user.rb
+++ rails/alpha-blog/app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   validates :name, :username, :email, presence:true
   validates :name, format: { with: /\A[a-zA-Z]+\z/, message: "name is not valid." }
   validates :username, format: { with: /\A\w.+\z/, message: "Enter user name in form of 'user1234'."}
-  validates :email, format: { with: /\A[\w.+-]+@\w+\.\w+\z/, message: "Enter correct email format." }
+  validates :email, format: { with: /\A[\w.+-]+@\w+\.\w+\z/, message: "Enter correct email format." }, uniqueness: true
   validates :gender, inclusion: { in: %w(male female other), message: "Not valid gender."}
   validates :name, length: { minimum: 4, maximum:10}
   validates :ph_no, numericality: { only_integer: true}
