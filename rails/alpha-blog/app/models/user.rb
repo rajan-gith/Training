80178e60d75885ac1e271b59cf79d8e6785ba17a
rajan-gith <rajan.prajapati@berylsystems.com>
2019-03-20 11:49:32 +0530

    name validate to only string

--- rails/alpha-blog/app/models/user.rb
+++ rails/alpha-blog/app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   has_many :comments, as: :commentable
 
   validates :name, :username, :email, presence:true
-  validates :name, format: { with: /\A\w.+\z/, message: "name is not valid." }
+  validates :name, format: { with: /\A\[a-zA-Z]+\z/, message: "name is not valid." }
   validates :username, format: { with: /\A\w.+\z/, message: "Enter user name in form of 'user1234'."}
   validates :email, format: { with: /\A[\w.+-]+@\w+\.\w+\z/, message: "Enter correct email format" }
 end
