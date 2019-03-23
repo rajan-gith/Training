7d9e157134c62758771c88b0822bd77c164d1adf
rajan-gith <rajan.prajapati@berylsystems.com>
2019-03-23 11:09:36 +0530

    In Topic model title value assigned from callback

--- rails/alpha-blog/app/models/topic.rb
+++ rails/alpha-blog/app/models/topic.rb
@@ -1,3 +1,10 @@
 class Topic < ApplicationRecord
   validates :title, length: { minimum:5, allow_nil:false, message: "title can't be nill or less than 5" }
+  before_validation :ensure_topic_is_there
+
+  def ensure_topic_is_there
+    if title.nil?
+      self.title = "assigned form callback"
+    end
+  end
 end
