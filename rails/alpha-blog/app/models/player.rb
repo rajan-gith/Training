dbd76e8d9627752c6d14995f9e97d5090e321f35
rajan-gith <rajan.prajapati@berylsystems.com>
2019-03-23 16:14:55 +0530

    Increment in players count by usnig after_create callback in Player model

--- rails/alpha-blog/app/models/player.rb
+++ rails/alpha-blog/app/models/player.rb
@@ -1,3 +1,12 @@
 class Player < ApplicationRecord
   belongs_to :team
+
+  after_create :update_no_of_players
+
+  def update_no_of_players
+    puts ("count before #{self.team.players_count} ")
+    self.team.players_count += 1
+    puts ("count after #{self.team.players_count} ")
+    self.team.save
+  end
 end
