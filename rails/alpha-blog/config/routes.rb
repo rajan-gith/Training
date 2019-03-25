f878829b309ed1a95bbdad7c33b6fd7a3e6dba2e
rajan-gith <rajan.prajapati@berylsystems.com>
2019-03-25 10:10:52 +0530

    Removed Student routes

--- rails/alpha-blog/config/routes.rb
+++ rails/alpha-blog/config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  get 'student/index'
-  get 'student/edit'
-  get 'student/new'
-  get 'student/delete'
-  get 'student/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
   get 'about', to: 'welcome#about'
