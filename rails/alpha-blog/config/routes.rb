1344caf8605c12008ed54c63557b0ac544570492
rajan-gith <rajan.prajapati@berylsystems.com>
2019-03-26 15:40:35 +0530

    Fix: Rout missmatch bug fixed

--- rails/alpha-blog/config/routes.rb
+++ rails/alpha-blog/config/routes.rb
@@ -11,9 +11,8 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'about', to: 'welcome#about'
   get 'user', to: 'user#user_list'
-
+  get 'user/new', to: 'user#new', as: 'new_user'
   get 'user/:id', to: 'user#show', as: 'show_user'
-  get 'user/new', to: 'user#new', as: 'new_user'  
   get 'user/delete/:id', to: 'user#delete', as: 'delete_user'
   post 'user/create/', to: 'user#create', as: 'create_user'
   get 'user/edit/:id', to: 'user#edit', as: 'edit_user'
