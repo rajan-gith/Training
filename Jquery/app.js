4165f7b9724598ea78f72c752d3249106a01122b
rajan-gith <rajan.prajapati@berylsystems.com>
2019-03-27 16:21:58 +0530

    selecting nav with id nav and class of article_item

--- Jquery/app.js
+++ Jquery/app.js
@@ -1,4 +1,10 @@
 // list of all div tags elements
 
 var div_collection;
-div_collection = $('div') 
+div_collection = $('div')
+
+var class_collection
+class_collection = $('.article-item')
+
+var nav
+nav = $('#nav')
