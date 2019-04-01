f05c9a3db01adc08fb58d4b97ee34880e51c9d5d
rajan-gith <rajan.prajapati@berylsystems.com>
2019-04-01 12:44:41 +0530

    Books Model books model added and controller genrate for book.

---
+++ rails/Devise_demo_project/db/migrate/20190401071021_create_books.rb
@@ -0,0 +1,11 @@
+class CreateBooks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :books do |t|
+      t.string :name
+      t.integer :price
+      t.references :users, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
