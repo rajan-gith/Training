a3aa7a669d76aa887b9d0f4ae75c0286330b6169
rajan-gith <rajan.prajapati@berylsystems.com>
2019-04-02 14:29:10 +0530

    Testing behaviour style property use imagemagic to manipulate images to a specific tag and format and size.

--- rails/Uploader_Management/app/controllers/photos_controller.rb
+++ rails/Uploader_Management/app/controllers/photos_controller.rb
@@ -1,5 +1,5 @@
 class PhotosController < ApplicationController
-
+  
   def index
     @photos = Photo.order('created_at')
   end
--- rails/Uploader_Management/app/models/photo.rb
+++ rails/Uploader_Management/app/models/photo.rb
@@ -1,5 +1,5 @@
 class Photo < ApplicationRecord
-  has_attached_file :image, styles: { thumb: ["64x64#", :jpg] }
+  has_attached_file :image, styles: { thumb: ["64x64#", :jpg], cropped: ["64x64#", :jpg] }
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif", "application/pdf"]
 
 end
