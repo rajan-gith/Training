518aef058b45a246e1f028dc22c25f6121967a7c
rajan-gith <rajan.prajapati@berylsystems.com>
2019-04-04 19:14:10 +0530

    Partial added for list of products

--- rails/shop_management/app/controllers/products_controller.rb
+++ rails/shop_management/app/controllers/products_controller.rb
@@ -60,7 +60,6 @@ class ProductsController < ApplicationController
       format.json { head :no_content }
     end
   end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_product
