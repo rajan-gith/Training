35d9b2874a50ab66ca521afc856d80ffbb76ff82
rajan-gith <rajan.prajapati@berylsystems.com>
2019-04-05 18:53:47 +0530

    Editing serializers

--- rails/cr_library/app/controllers/api/v1/authors_controller.rb
+++ rails/cr_library/app/controllers/api/v1/authors_controller.rb
@@ -4,7 +4,11 @@ module Api
       protect_from_forgery with: :null_session
       def index
         @authors = Author.all
-        render json: @authors
+        # render json: @authors
+        # render :json => { Author: AuthorSerializer.new(@authors , root: false, serializer_options: {bookName: "books"}), :status => 200}
+        # render json: serializer: AuthorSerializer
+        # render json: @authors, adapter: :json, serializer_options: {bookName: "1234"},status: 200
+        render json: @authors, adapter: :json, serializer_options: {bookName: params[:book_name]},status: 200
       end
       def create
         # debugger
--- rails/cr_library/app/serializers/author_serializer.rb
+++ rails/cr_library/app/serializers/author_serializer.rb
@@ -1,3 +1,9 @@
 class AuthorSerializer < ActiveModel::Serializer
   attributes :id, :first_name, :last_name
+  # def attributes(*args)
+  #   a = ""
+  #   data = super
+  #   data[:test] = "hello"
+  #   data
+  # end
 end
