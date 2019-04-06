ef61d0773d00bec79de91345d05b742b345a7b0e
rajan-gith <rajan.prajapati@berylsystems.com>
2019-04-06 13:02:32 +0530

    Routes defined for omniauth facebook

--- rails/Book_Store/config/routes.rb
+++ rails/Book_Store/config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   delete 'book/delete'
   get 'book/edit'
   get 'book/current'
-  devise_for :users
+  # devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
