eaffd664dffd5fefed5a8b8867ce65e3d47b2691
rajan-gith <rajan.prajapati@berylsystems.com>
2019-04-06 16:20:59 +0530

    Removed comment

--- rails/shop_management/config/routes.rb
+++ rails/shop_management/config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # get '/products/pagination' => 'products#search', as: 'paging'
   get 'products/search', to: 'products#search', as: 'search'
   resources :products
 end
