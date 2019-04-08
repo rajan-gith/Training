291130a734a4b6054d7906fbeca3d11cc33d12eb
rajan-gith <rajan.prajapati@berylsystems.com>
2019-04-08 12:17:29 +0530

    routes and controller added

--- rails/profile_view/config/routes.rb
+++ rails/profile_view/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  post 'users/create'
+  patch 'users/update'
+  delete 'users/delete'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
