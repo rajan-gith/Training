3b847076bdb515d2c8ce2c79caa575309144a59a
rajan-gith <rajan.prajapati@berylsystems.com>
2019-04-08 15:48:27 +0530

    managed api version structure

--- rails/profile_view/config/routes.rb
+++ rails/profile_view/config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  post 'users/create'
-  patch 'users/update'
-  delete 'users/delete'
+  namespace :api do
+    namespace :v1 do
+      get 'users/index'
+      get 'users/show'
+      post 'users/create'
+      patch 'users/update'
+      delete 'users/delete'
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
