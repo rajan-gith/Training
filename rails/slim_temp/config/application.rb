d172844008a7ac2e70fbfcdd39c566f0d38f2939
rajan-gith <rajan.prajapati@berylsystems.com>
2019-04-09 11:30:24 +0530

    slim gem configuration added

--- rails/slim_temp/config/application.rb
+++ rails/slim_temp/config/application.rb
@@ -21,7 +21,9 @@ module SlimTemp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.generators do |g|
+       g.template_engine :slim
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
