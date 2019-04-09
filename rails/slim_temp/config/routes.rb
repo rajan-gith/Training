a391594eb5c46455ee9654de7341d25811527376
rajan-gith <rajan.prajapati@berylsystems.com>
2019-04-09 15:48:11 +0530

    Delete route

--- rails/slim_temp/config/routes.rb
+++ rails/slim_temp/config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'posts/update'
   post 'posts/create'
   root 'welcome#index'
+  delete 'posts/delete', to: "posts#delete", as: 'posts_delete'
   # get 'welcome/about'
   # get 'welcome/contact_us'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
