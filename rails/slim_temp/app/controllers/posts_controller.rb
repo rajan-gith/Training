a52f111181e4a74be304ecdab0711a8654baa91e
rajan-gith <rajan.prajapati@berylsystems.com>
2019-04-09 15:11:15 +0530

    Post controller adding posts

--- rails/slim_temp/app/controllers/posts_controller.rb
+++ rails/slim_temp/app/controllers/posts_controller.rb
@@ -3,11 +3,21 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
+    debugger
   end
 
   def update
   end
 
   def create
+    @post = Post.new(post_param)
+    @post.save
+    redirect_to root_path
+  end
+  
+  private
+  def post_param
+    params.require(:post).permit(:title,:body)
   end
 end
