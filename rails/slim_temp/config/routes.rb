bc3d329e6f31641de4824a240bae19d6d2b259e3
rajan-gith <rajan.prajapati@berylsystems.com>
2019-04-09 15:14:03 +0530

    Posts create route

--- rails/slim_temp/config/routes.rb
+++ rails/slim_temp/config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   get 'posts/edit'
   get 'posts/new'
   get 'posts/update'
-  get 'posts/create'
-  get 'welcome/index'
+  post 'posts/create'
+  root 'welcome#index'
   # get 'welcome/about'
   # get 'welcome/contact_us'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
