b101aa2d1d0907db68373d6f65cbeb4f77e47dbe
rajan-gith <rajan.prajapati@berylsystems.com>
2019-04-10 12:59:37 +0530

    Javascript added

---
+++ rails/policy_management/app/assets/javascripts/campaigns.js
@@ -0,0 +1,20 @@
+function next_click_behave() {
+  $('.btnNext').click(function() {
+    $('.nav-pills > .nav-item > .active').parent().next('li').find('a').trigger('click');
+    $('.nav-pills > .nav-item > .active').parent().prev('li').find('a').addClass('completed');
+    $('.nav-pills > .nav-item > .active').parent().prev('li').find('.wizard_line').addClass('completed');
+  });
+  $('.btnPrevious').click(function() {
+    $('.nav-pills > .nav-item > .active').parent().prev('li').find('a').trigger('click');
+    $('.nav-pills > .nav-item > .active').parent().next('li').find('a').removeClass('completed');
+    $('.nav-pills > .nav-item > .active').parent().next('li').find('.wizard_line').removeClass('completed');
+  });
+  $("#side_display").click(function(){
+    $(".left_nav").addClass("active");
+  });
+  $(".side_close").click(function(){
+    $(".left_nav").removeClass("active");
+  });
+}
+
+$(next_click_behave)
