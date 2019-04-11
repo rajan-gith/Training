c6eee611c56342ef5ed1f10096d94055d5ad66cf
rajan-gith <rajan.prajapati@berylsystems.com>
2019-04-11 12:08:26 +0530

    google_oauth2 callback request handler

--- rails/Book_Store/app/controllers/users/omniauth_callbacks_controller.rb
+++ rails/Book_Store/app/controllers/users/omniauth_callbacks_controller.rb
@@ -12,6 +12,17 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     end
   end
 
+  def google_oauth2
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication
+    else
+      session["devise.google_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
+    end
+    # redirect_to '/'
+  end
+
   def failure
     redirect_to root_path
   end
