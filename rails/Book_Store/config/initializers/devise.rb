67a7bed6dfe12ece42ab18d8de715c55b8910f07
rajan-gith <rajan.prajapati@berylsystems.com>
2019-04-11 12:05:57 +0530

    Client id and client secret added for google oauth

--- rails/Book_Store/config/initializers/devise.rb
+++ rails/Book_Store/config/initializers/devise.rb
@@ -297,4 +297,6 @@ Devise.setup do |config|
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
   config.omniauth :facebook, "APP_ID", "APP_SECRET", callback_url: "http://localhost:3000/users/auth/facebook/callback"
+  config.omniauth :google_oauth2,"Client_id", "Client_secret" #redirect_uri: "http://localhost:3000/users/auth/google/callback"
+
 end
