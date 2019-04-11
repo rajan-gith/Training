7ab7660fd146bcc6d09c6c4304a1e1cfa72cf9ba
rajan-gith <rajan.prajapati@berylsystems.com>
2019-04-11 12:06:59 +0530

    google oauth added to devise in User model

--- rails/Book_Store/app/models/user.rb
+++ rails/Book_Store/app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: %i[facebook]
+         :omniauthable, :omniauth_providers =>[:facebook , :google_oauth2]
   has_many :books
   def self.from_omniauth(auth)
     # debugger
