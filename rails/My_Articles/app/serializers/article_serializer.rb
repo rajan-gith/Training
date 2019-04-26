82905c2dc2c15c22ea07c5045fc0197267e4cf4b
rajan-gith <rajan.prajapati@berylsystems.com>
2019-04-26 18:15:22 +0530

    added date in serializer

--- rails/My_Articles/app/serializers/article_serializer.rb
+++ rails/My_Articles/app/serializers/article_serializer.rb
@@ -1,3 +1,8 @@
 class ArticleSerializer < ActiveModel::Serializer
   attributes :id, :title, :body
+  def attributes(*args)
+    data = super
+    data[:created_at] = object.created_at
+    data
+  end
 end
